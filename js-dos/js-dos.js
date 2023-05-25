e81903d03844142379bd08508b3ac6f96089820a
c-sean <45257662+c-sean@users.noreply.github.com>
2023-05-25 14:47:57 +0800

    Update js-dos.js

--- js-dos/js-dos.js
+++ js-dos/js-dos.js
@@ -5041,7 +5041,7 @@
                             this.clear(), this.initialized || this.beforeFirstRender(), this.beforeRender();
                             var t = "hg-layout-".concat(this.options.layoutName),
                                 n = this.options.layout || {
-                                    default: ["` 1 2 3 4 5 6 7 8 9 0 - = {bksp}", "{tab} q w e r t y u i o p [ ] \\", "{lock} a s d f g h j k l ; ' {enter}", "{shift} z x c v b n m , . / {shift}", ".com @ {space}", "{arrowup} {arrowdown} {arrowleft} {arrowright}"],
+                                    default: ["` 1 2 3 4 5 6 7 8 9 0 - = {bksp}", "{tab} q w e r t y u i o p [ ] \\", "{lock} a s d f g h j k l ; ' {enter}", "{shift} z x c v b n m , . / {shift}", ".com @ {space}", "{arrowup} {arrowdown} {arrowleft} {arrowright}"], 
                                     shift: ["~ ! @ # $ % ^ & * ( ) _ + {bksp}", "{tab} Q W E R T Y U I O P { } |", '{lock} A S D F G H J K L : " {enter}', "{shift} Z X C V B N M < > ? {shift}", ".com @ {space}"]
                                 },
                                 r = this.options.useTouchEvents || !1,
